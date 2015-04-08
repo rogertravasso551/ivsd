608cbd240830c69823ca60ee1f89c944f365c0ad
SaniyaMeher <sadika.shabbir@gmail.com>
2015-04-08 16:17:43 +0530

    Update frameheader.php

--- frameheader.php
+++ frameheader.php
@@ -38,4 +38,44 @@ font-weight: 300;font-style: italic;">Life Easy</small>
                     </ol>
                         
                 </section>
+				</aside><header class="header" style="position: absolute;max-height: 100px;z-index: 1030;display: block;top: 0;
+left: 0;
+right: 0;">
+            <a href="dashboard.php" target="_top" class="logo" style="float: left;
+font-size: 20px;
+line-height: 50px;
+text-align: center;
+padding: 0 10px;
+width: 196px;
+font-family: 'Kaushan Script', cursive;
+font-weight: 500;
+height: 57px;
+display: block;background-color: #367fa9;
+color: #f9f9f9;text-decoration: none;">
+                <!-- Add the class icon to your logo image or logo icon to add the margining -->
+                VISUALIZATION
+            </a>
+            <!-- Header Navbar: style can be found in header.less -->
+           
+        </header>
+		<aside style="background-color: #f9f9f9;
+margin-left: 210px;min-height: 100%;
+display: block;">
+		<section class="content-header" style="background: #fbfbfb;
+box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1);margin-top: -6px;
+height: 27px;
+padding: 15px 15px 10px 20px;position:relative;">
+                    <h1 style="margin: 0;font-size: 24px;font-style: italic;font-weight:500px;">
+                        Makes
+                        <small style="font-size: 15px;
+display: inline-block;
+padding-left: 4px;
+font-weight: 300;font-style: italic;">Life Easy</small>
+                    </h1>
+         <ol class="breadcrumb">
+                        
+                        
+                    </ol>
+                        
+                </section>
 				</aside>
